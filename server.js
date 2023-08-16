f60838b762e1ea69d79471e622b3412f0e43c5b5
wkdtpzld <slwhswk@naver.com>
2023-08-16 18:10:20 +0900

    📦️ graphql 설치 및 초기 튜토리얼

---
+++ server.js
@@ -0,0 +1,20 @@
+const {ApolloServer, gql} = require("apollo-server");
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello: () => 'bwbw',
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(() => console.log("server is running on http://localhost:4000/"));
