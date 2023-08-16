73d983559772177177287fce7526ba294831d38c
wkdtpzld <slwhswk@naver.com>
2023-08-16 19:49:24 +0900

    📝 Graphql 복습 튜토리얼

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 import {gql, ApolloServer} from "apollo-server"
 
 const typeDefs = gql`
+    type Movie {
+        title: String
+        year: Int
+    },
     type Query {
-        hello: String
+        movies: [Movie]
+        movie: Movie
+    },
+    type Mutation {
+        createMovie(title: String!): Boolean
+        deleteMovie(title: String!): Boolean
     }
 `
 
 const resolvers = {
     Query: {
-        hello: () => 'bwbw',
+        movies: () => [],
+        movie: () => ({"title": "Hello", year: 2021})
+    },
+    Mutation: {
+        createMovie: (_, args) => {
+            console.log(args);
+            return true;
+        },
+        deleteMovie: (_, args) => {
+            console.log(args);
+            return true;
+        },
     }
 };
 
