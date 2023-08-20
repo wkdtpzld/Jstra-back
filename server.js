211b16c8bb286d9dd33daac4460539ae51df0464
wkdtpzld <slwhswk@naver.com>
2023-08-20 22:14:11 +0900

    📝 graphql 기본 튜토리얼

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 import {gql, ApolloServer} from "apollo-server"
+import {PrismaClient} from "@prisma/client";
+
+const client = new PrismaClient();
 
 const typeDefs = gql`
     type Movie {
-        title: String
-        year: Int
+        id: Int
+        title: String!
+        year: Int!
+        genre: String
+        createdAt: String!
+        updatedAt: String!
     },
     type Query {
         movies: [Movie]
-        movie: Movie
+        movie(id: Int!): Movie
     },
     type Mutation {
-        createMovie(title: String!): Boolean
-        deleteMovie(title: String!): Boolean
+        createMovie(title: String!, year: Int!, genre: String): Movie
+        deleteMovie(id: Int!): Boolean
     }
 `
 
 const resolvers = {
     Query: {
-        movies: () => [],
-        movie: () => ({"title": "Hello", year: 2021})
+        movies: () => client.movie.findMany(),
+        movie: (_, {id}) => client.movie.findUnique({where: { id }})
     },
     Mutation: {
-        createMovie: (_, args) => {
-            console.log(args);
-            return true;
+        createMovie: (_, { title, year, genre }) => {
+            client.movie.create({ data: {title, year, genre} });
         },
-        deleteMovie: (_, args) => {
-            console.log(args);
-            return true;
-        },
+        deleteMovie: (_, { id }) => {
+            client.movie.delete({where: {id}});
+        }
     }
 };
 
